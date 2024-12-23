a31c845aa144946e655fe0bb79d18e32a58420ce
Divya Prakash <prakashpreveen@gmail.com>
2024-12-23 19:51:33 +0530

    added hero and card section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,20 +2,21 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <div>
-      <div>
+    <div className="max-w-[1640px] max-auto p-4 ">
+      <div className="max-h-[500px] relative">
         {/* OverLay */}
-        <div>
-          <h1>
-            The <span>Best</span>
+        <div className="absolute w-full h-full bg-black/40 max-f-[500px] text-gray-200 flex flex-col text-2xl justify-center">
+          <h1 className="px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold">
+            The <span className="text-orange-500">Best</span>
           </h1>
-          <h1>
-            Foods <span>Delivered</span>
+          <h1 className="px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold">
+            Delivered <span className="text-orange-500">Foods</span>
           </h1>
         </div>
         <img
-          src="https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          src="https://images.pexels.com/photos/1639562/pexels-photo-1639562.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
           alt=""
+          className="w-full max-h-[500px] object-cover"
         />
       </div>
     </div>
